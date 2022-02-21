7f807b86bf58fe1b66e907c2595111d309c453b0
Devon Kozenieski <devonk@iri.com>
2022-02-21 10:47:29 -0500

    Allow selection of multiple files

--- ajax.js
+++ ajax.js
@@ -38,13 +38,15 @@ function sendRequest() {
     let result = document.querySelector('.result');
     let response = document.querySelector('.responseCode');
     // Creating a XHR object
-    let xhr = new XMLHttpRequest();
     let url = "http://" + getElementValue("host") + ":" + getElementValue("port") + "/api/darkshield/" + getElementValue("endpoint");
 
-    // open a connection
+    let files = document.getElementById("formFile").files;
+
+    console.log(files.length);
+    if (files.length <= 1){
+        let xhr = new XMLHttpRequest();
+            // open a connection
     xhr.open("POST", url, true);
-
-
     // Create a state change callback
     xhr.onreadystatechange = function () {
         makeElementInvisible("blueLoadSpin")
@@ -140,4 +142,107 @@ function sendRequest() {
             break;
     }
     makeElementVisible("blueLoadSpin")
+}
+else if (files.length > 1){
+    for (let i=0; i<files.length; i++) {
+        let xhr = new XMLHttpRequest();
+            // open a connection
+            
+    xhr.open("POST", url, true);
+     // Create a state change callback
+     xhr.onreadystatechange = function () {
+        makeElementInvisible("blueLoadSpin")
+        response.innerHTML = xhr.status;
+        console.log(xhr.status);
+        endPoint = getElementValue("endpoint");
+        switch (endPoint) {
+            case "searchContext.create":
+
+            case "searchContext.destroy":
+
+            case "maskContext.create":
+
+            case "maskContext.destroy":
+
+            case "searchContext.search":
+
+            case "searchContext.mask":
+
+            case "maskContext.mask":
+
+            case "files/fileSearchContext.create":
+
+            case "files/fileMaskContext.create":
+
+            case "files/fileSearchContext.destroy":
+            case "files/fileSearchContext.search":
+            case "files/fileMaskContext.destroy":
+                result.innerHTML = this.responseText;
+                break;
+            case "files/fileMaskContext.mask":
+            case "files/fileSearchContext.mask":
+                parts = parseMultipartResponse(new Uint8Array(xhr.response));
+                if (parts.length == 0) {
+                    break;
+                }
+                var maskedFileUrl = URL.createObjectURL(new Blob([parts[1].buffer])); // parts[1] should be the masked file, parts[0] should be the JSON results file.
+                var resultsFileUrl = URL.createObjectURL(new Blob([parts[0].buffer])); // parts[1] should be the masked file, parts[0] should be the JSON results file.
+                var link = document.createElement("a");
+                link.href = maskedFileUrl;
+                link.download = "masked_" + document.getElementById("formFile").files[i].name;
+                link.click()
+                link.href = resultsFileUrl;
+                link.download = document.getElementById("formFile").files[i].name + "_results.json";
+                link.click()
+                break;
+            default:
+                break;
+        }
+    };
+
+    endPoint = getElementValue("endpoint");
+    switch (endPoint) {
+        case "searchContext.create":
+
+        case "searchContext.destroy":
+
+        case "maskContext.create":
+
+        case "maskContext.destroy":
+
+        case "searchContext.search":
+
+        case "searchContext.mask":
+
+        case "maskContext.mask":
+
+        case "files/fileSearchContext.create":
+
+        case "files/fileMaskContext.create":
+
+        case "files/fileSearchContext.destroy":
+
+        case "files/fileMaskContext.destroy":
+            // Set the request header i.e. which type of content you are sending
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(document.getElementById("payloadText").textContent);
+            break;
+        case "files/fileSearchContext.search":
+        case "files/fileMaskContext.mask":
+        case "files/fileSearchContext.mask":
+            var formData = new FormData();
+            formData.append("context", document.getElementById("payloadText").textContent);
+            formData.append("file", document.getElementById('formFile').files[i]);
+            if (endPoint != "files/fileSearchContext.search") {
+                xhr.responseType = "arraybuffer";
+            }
+            xhr.send(formData);
+            break;
+        default:
+            break;
+    }
+    makeElementVisible("blueLoadSpin")
+    }
+}
+
 }
